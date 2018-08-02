ea87ce79588be613c75fb81b533e0f6e437bbd05
이언석 <djstjr@naver.com>
2018-08-02 17:10:24 +0900

    splash 추가

---
+++ app/src/main/java/com/example/wjdck/hakerton/splashActivity.java
@@ -0,0 +1,30 @@
+package com.example.wjdck.hakerton;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class splashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getBaseContext(), loginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+        },2000);
+    }
+
+}
