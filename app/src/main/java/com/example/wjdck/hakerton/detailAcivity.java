a12b4d875792eee6855bbaac9c916b998a7de48f
201324511 <jwj6258@naver.com>
2018-08-06 15:53:44 +0900

    splash, linearlayout

--- app/src/main/java/com/example/wjdck/hakerton/detailAcivity.java
+++ app/src/main/java/com/example/wjdck/hakerton/detailAcivity.java
@@ -6,6 +6,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 
 public class detailAcivity extends AppCompatActivity {
 
@@ -13,6 +18,9 @@ public class detailAcivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+
+
 
     }
 
