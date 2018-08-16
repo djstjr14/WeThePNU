f6aece7aa9f420b01b1dcdb5ef1bbc57859a4f5a
이언석 <djstjr@naver.com>
2018-08-16 14:20:17 +0900

    splash 수정

--- app/src/main/java/com/example/wjdck/hakerton/splashActivity.java
+++ app/src/main/java/com/example/wjdck/hakerton/splashActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 public class splashActivity extends AppCompatActivity {
 
