1f5c2fa0b2ac68d453d92c9f3546dfc130ccce2f
YuEun Lee <lye4191@naver.com>
2018-08-22 21:03:04 +0900

    floating btn

--- app/src/main/java/com/example/wjdck/hakerton/MainActivity.java
+++ app/src/main/java/com/example/wjdck/hakerton/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -15,17 +14,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Locale;
+import com.melnykov.fab.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar_main;
     DrawerLayout drawer;
     NavigationView navigation;
+
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +51,9 @@ public class MainActivity extends AppCompatActivity {
 
         adapter = new ListViewAdapter();
         listView.setAdapter(adapter);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.attachToListView(listView);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -123,6 +123,16 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         mDatabaseReference.addChildEventListener(mChildEventListener);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, addAgendaActivity.class);
+                //intent.putExtra("");
+                startActivity(intent);
+
+            }
+        });
     }
 
     //추가된 소스, ToolBar에 main.xml을 인플레이트함
