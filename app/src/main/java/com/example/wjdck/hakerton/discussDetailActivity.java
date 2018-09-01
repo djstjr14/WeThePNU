8a09cc16f92d9ac9c121ef87a055133875fabb3d
YuEun Lee <lye4191@naver.com>
2018-09-01 19:38:24 +0900

    토론게시판 디테일 xml 수정

--- app/src/main/java/com/example/wjdck/hakerton/discussDetailActivity.java
+++ app/src/main/java/com/example/wjdck/hakerton/discussDetailActivity.java
@@ -1,18 +1,122 @@
 package com.example.wjdck.hakerton;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
+import static com.example.wjdck.hakerton.loginActivity.Uid;
+import static com.example.wjdck.hakerton.loginActivity.appData;
+
 public class discussDetailActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    NavigationView navigation;
+    DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discuss_detail);
+
+        toolbar = findViewById(R.id.discuss_detail_toolbar);
+        navigation = findViewById(R.id.navigation_discuss_detail);
+        drawer = findViewById(R.id.drawer_discuss_detail);
+
+        //Toolbar 추가
+
+        setSupportActionBar(toolbar);
+        //Toolbar의 왼쪽에 뒤로가기 버튼을 추가
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.baseline_arrow_back_ios_white_18dp);
+
+        navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                drawer.closeDrawers();
+
+                int id = item.getItemId();
+                switch(id){
+                    case R.id.navigation_item1:
+                        Intent intent1 = new Intent(discussDetailActivity.this, MyListActivity.class);
+                        intent1.putExtra("OPTION", 1);
+                        startActivity(intent1);
+                        break;
+
+                    case R.id.navigation_item2:
+                        Intent intent2 = new Intent(discussDetailActivity.this, MyListActivity.class);
+                        intent2.putExtra("OPTION", 2);
+                        startActivity(intent2);
+                        break;
+
+                    case R.id.navigation_item3:
+                        Intent intent3 = new Intent(discussDetailActivity.this, discussActivity.class);
+                        startActivity(intent3);
+                        break;
+
+                    case R.id.navigation_item4:
+                        break;
+
+                    case R.id.navigation_item5:
+                        Intent intent5 = new Intent(discussDetailActivity.this, loginActivity.class);
+                        Uid = null;
+                        startActivity(intent5); SharedPreferences.Editor editor= appData.edit();
+                        editor.clear().apply();
+                        startActivity(intent5);
+                        break;
+                }
+
+                return true;
+            }
+        });
+    }
+
+
+    //추가된 소스, ToolBar에 main.xml을 인플레이트함
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //return super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //추가된 소스, ToolBar에 추가된 항목의 select 이벤트를 처리하는 함수
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case android.R.id.home:{
+                finish();
+                return true;
+            }
+            case R.id.action_menu:{
+                drawer.openDrawer(GravityCompat.END);
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_discuss_detail);
+        if (drawer.isDrawerOpen(GravityCompat.END)) {
+            drawer.closeDrawer(GravityCompat.END);
+        } else {
+            super.onBackPressed();
+        }
     }
 
 }
